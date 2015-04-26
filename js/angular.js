92b11fd827b13e9d8514300559e85260276ae15f
Brian Landry <brian.landry@gmail.com>
2015-04-25 22:56:59 -0500

    Devices now load from file

--- js/angular.js
+++ js/angular.js
@@ -1,42 +1,11 @@
 var app = angular.module('myApp', []);
 app.controller('ctrl', function($scope) {
     $scope.leds=[];
-    $scope.devices=[
-        {
-            name:'Two Color Array',
-            rows: 8,
-            cols: 12,
-            leds: [630, 660],
-            display: 'none'
-        },{
-            name:'Light Plate Apparatus',
-            rows: 4,
-            cols: 6,
-            leds: [630, 660],
-            display: 'none'
-        },
-        {
-            name:'Light Tube Array',
-            rows: 8,
-            cols: 8,
-            leds: [630, 660],
-            display: 'none'
-        },
-        {
-            name:'OptoGeneSat',
-            rows: 4,
-            cols: 12,
-            leds: [630, 660],
-            display: 'none'
-        },
-        {
-            name:'Custom Configuration',
-            rows: 4,
-            cols: 6,
-            leds: [],
-            display: 'block'
-        }
-    ];
+    //Loads devices from file
+    $.getJSON("devices.json", function(json) {
+        console.log(json); // this will show the info it in firebug console
+        $scope.devices = json;
+    });
 
     //Utility function to repeat X number of times
     $scope.getNumber = function(num) {
