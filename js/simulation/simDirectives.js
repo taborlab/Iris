3a68f9a32db5187b32b856cc1a6326f1cd269eb8
Brian Landry <brian.landry@gmail.com>
2015-05-11 22:02:11 -0500

    #166

--- js/simulation/simDirectives.js
+++ js/simulation/simDirectives.js
@@ -27,6 +27,7 @@ app.directive('keyTrap', function() {
     return function( scope, elem ) {
         elem.bind('keydown', function( event ) {
             scope.$broadcast('keydown', { code: event.keyCode } );
+            console.log(event);
         });
     };
 });
