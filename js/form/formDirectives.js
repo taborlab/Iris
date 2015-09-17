11d63b8e2c5d2203a29e48ee74979ecd06e44c48
Brian Landry <brian.landry@gmail.com>
2015-09-17 18:29:24 -0500

    Fixed #199

--- js/form/formDirectives.js
+++ js/form/formDirectives.js
@@ -48,7 +48,7 @@ app.directive('myWaveform',['$compile', '$templateCache', function ($compile, $t
                     var cellProperties = {}
                     if (row === 0 && col === 0) {
                         cellProperties.readOnly = true;
-                        type = "string"
+                        cellProperties.type = "text"
                     }
                     return cellProperties;
                 },
