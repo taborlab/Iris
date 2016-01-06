14ff8d046012c5397335ee8a07aa5f381fb776f1
Brian Landry <brian.landry@gmail.com>
2016-01-05 21:39:53 -0600

    Zip file now displays time in a readable manner

--- js/form/formServices.js
+++ js/form/formServices.js
@@ -1274,9 +1274,13 @@ function Plate(data) {
         var content = zip.generate({type: "blob"});
 
         var d = new Date();
-        var filename = d.getFullYear() + ("0" + (d.getMonth() + 1)).slice(-2)
+        var filename = d.getFullYear()
+            + ("0" + (d.getMonth() + 1)).slice(-2)
             + ("0" + d.getDate()).slice(-2)
-            + "_" + d.getTime();
+            + "_"
+            + ("0" + d.getHours()).slice(-2)
+            + ("0" + d.getMinutes()).slice(-2)
+            + ("0" + d.getSeconds()).slice(-2);
         saveAs(content, filename);
     }
 
